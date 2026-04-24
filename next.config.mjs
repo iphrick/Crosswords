8bafd120a91a1bab51328087e5c36619ccc20f6c
Pedro Henrique <pedrohenriqueinsec281@gmail.com>
2026-04-23 21:07:22 -0300

    Atualização do projeto

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,12 @@ const nextConfig = {
   images: {
     domains: ['minotar.net'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
